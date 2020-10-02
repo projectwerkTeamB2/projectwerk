a67f0746f1d04853448f1a71a3f4765a2473090e
vladik kozynin <liekesliefhebber2000@hotmail.com>
2020-10-02 11:29:48 +0200

    wegschijven naar databank start

---
+++ StripcatalogusApp/Datalaag/wegschijvenNaarDatabanken.cs
@@ -0,0 +1,15 @@
+﻿using businesslaag;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datalaag
+{
+    public class wegschijvenNaarDatabanken
+    {
+        public void wegschijvenNaarDataBank(List<Strip> strips)
+        {
+
+        }
+    }
+}
