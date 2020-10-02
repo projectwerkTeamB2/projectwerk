bfd25cc9690983adc55f92ad82c3f59bd91418d1
Crow Scare <ciki3100@gmail.com>
2020-10-02 22:11:28 +0200

    fix

---
+++ StripcatalogusApp/Datalaag2/Program.cs
@@ -0,0 +1,38 @@
+﻿using businesslaag;
+using Datalaag2.Providers;
+using Datalaag2.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Datalaag2
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            JsonFileReader_ToObjects jfr = new JsonFileReader_ToObjects();
+            List<Strip> stripsFromJson = jfr.leesJson_GeefAlleStripsTerug();
+            //    StripRepository databeheer = new StripRepository(@"Data Source =.\SQLEXPRESS; Initial Catalog = Labo; Integrated Security = True");
+
+
+
+            #region ljena's connectie code
+            DbProviderFactories.RegisterFactory("sqlserver", SqlClientFactory.Instance);
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=StripCatDB;Integrated Security=True";
+            DbProviderFactory sqlFactory = DbProviderFactories.GetFactory("sqlserver");
+
+
+
+
+            StripRepository sp = new StripRepository(sqlFactory, connectionString);
+            // sp.FindAll_strip();
+
+         //   sp.allesWegSchijvenNaarDataBank(stripsFromJson);
+            #endregion
+
+        }
+    }
+}
---
+++ StripcatalogusApp/Datalaag2/Repositories/StripRepository.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+using businesslaag;
+using Businesslaag;
+using Businesslaag.Repositories;
+
+namespace Datalaag2.Repositories
+{
+    public class StripRepository : IStripRepository
+    {
+    
+       private string connectionString;
+        #region
+           private DbProviderFactory sqlFactory;
+         public StripRepository(DbProviderFactory sqlFactory, string connectionString)
+        {
+            this.sqlFactory = sqlFactory;
+            this.connectionString = connectionString;
+        }
+        private DbConnection getConnection()
+        {
+            DbConnection connection = sqlFactory.CreateConnection();
+            connection.ConnectionString = connectionString;
+            return connection;
+        }
+        #endregion ljena connetion code
+
+        public void allesWegSchijvenNaarDataBank(List<Strip> strips)
+        {
+            #region maak dictionary's
+            Dictionary<int, Auteur> dictAuteurs = new Dictionary<int, Auteur>();
+            Dictionary<int, Uitgeverij> dictUitgeverij = new Dictionary<int, Uitgeverij>();
+            Dictionary<int, Reeks> dictReeks = new Dictionary<int, Reeks>();
+            int tellerAut = 1;
+            int tellerUit = 1;
+            int tellerRks = 1;
+            foreach (Strip s in strips)
+            {
+                foreach (Auteur a in s.Auteurs)
+                {
+                    if (!dictAuteurs.ContainsValue(a))
+                    {
+                        dictAuteurs.Add(tellerAut, a);
+                    }
+                }
+                if (!dictUitgeverij.ContainsValue(s.Uitgeverij))
+                {
+                    dictUitgeverij.Add(tellerUit, s.Uitgeverij);
+                }
+                if (!dictReeks.ContainsValue(s.Reeks))
+                {
+                    dictReeks.Add(tellerRks, s.Reeks);
+                }
+
+            }
+            #endregion
+            DbConnection connection = getConnection();
+            #region vul reeks op
+            string query0 = "INSERT INTO dbo.Reeks (Name)"
+                 + "VALUES(@Name)";
+            using (DbCommand command = connection.CreateCommand())
+            {
+                connection.Open();
+                try
+                {
+                    command.CommandText = query0;
+
+                    DbParameter Name = sqlFactory.CreateParameter();
+                    Name.ParameterName = "@Name";
+                    Name.DbType = DbType.String;
+                    command.Parameters.Add(Name);
+
+
+                    foreach (KeyValuePair<int, Reeks> dR in dictReeks)
+                    {
+                        command.Parameters["@Name"].Value = dR.Value.Naam;
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+            #endregion
+            #region vul Uitgeverij op
+            string query1 = "INSERT INTO dbo.Uitgeverij (Name)"
+                + "VALUES(@Name)";
+            using (DbCommand command = connection.CreateCommand())
+            {
+                connection.Open();
+                try
+                {
+                    command.CommandText = query1;
+
+                    DbParameter Name = sqlFactory.CreateParameter();
+                    Name.ParameterName = "@Name";
+                    Name.DbType = DbType.String;
+                    command.Parameters.Add(Name);
+
+
+                    foreach (KeyValuePair<int, Uitgeverij> dU in dictUitgeverij)
+                    {
+                        command.Parameters["@Titel"].Value = dU.Value.Naam;
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+            #endregion
+            #region vul Auteur op
+            string query3 = "INSERT INTO dbo.Auteur (Name)"
+                + "VALUES(@Name)";
+            using (DbCommand command = connection.CreateCommand())
+            {
+                connection.Open();
+                try
+                {
+                    command.CommandText = query3;
+
+                    DbParameter Name = sqlFactory.CreateParameter();
+                    Name.ParameterName = "@Name";
+                    Name.DbType = DbType.String;
+                    command.Parameters.Add(Name);
+
+
+                    foreach (KeyValuePair<int, Auteur> dA in dictAuteurs)
+                    {
+                        command.Parameters["@Titel"].Value = dA.Value.Naam;
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+            #endregion
+            #region vul Strip_has_Auteur
+            string query4 = "INSERT INTO dbo.Strip_has_Auteur (Strip_id,Auteur_id)"
+                + "VALUES(@Strip_id,@Auteur_id)";
+            using (DbCommand command = connection.CreateCommand())
+            {
+                connection.Open();
+                try
+                {
+                    command.CommandText = query4;
+
+                    DbParameter strip_id = sqlFactory.CreateParameter();
+                    strip_id.ParameterName = "@Strip_id";
+                    strip_id.DbType = DbType.Int32;
+                    command.Parameters.Add(strip_id);
+
+                    DbParameter Auteur_id = sqlFactory.CreateParameter();
+                    Auteur_id.ParameterName = "@Auteur_id";
+                    Auteur_id.DbType = DbType.Int32;
+                    command.Parameters.Add(Auteur_id);
+
+                    int teller = 1;
+                    foreach (Strip s in strips)
+                    {
+                        foreach (Auteur a in s.Auteurs)
+                        {
+                            foreach (KeyValuePair<int, Auteur> dA in dictAuteurs)
+                            {
+                                if (dA.Value.Naam == a.Naam)
+                                {
+                                    command.Parameters["@Strip_id"].Value = teller;
+                                    command.Parameters["@Auteur_id"].Value = dA.Value.Naam;
+                                    command.ExecuteNonQuery();
+                                }
+                            }
+                        }
+                        teller++;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+            #endregion
+            #region vul Strip op
+            string query2 = "INSERT INTO dbo.Strip (Titel,Nummer,Reeks_id,Uitgeverij_id)"
+                 + "VALUES(@Titel,@Nummer,@Reeks_id,@Uitgeverij_id)";
+            using (DbCommand command = connection.CreateCommand())
+            {
+                connection.Open();
+                try
+                {
+                    command.CommandText = query2;
+
+
+                    //      DbParameter id1 = sqlFactory.CreateParameter();
+                    //     id1.ParameterName = "@id";
+                    //      id1.DbType = DbType.Int32;
+                    //     command.Parameters.Add(id1);
+
+                    DbParameter Titel1 = sqlFactory.CreateParameter();
+                    Titel1.ParameterName = "@Titel";
+                    Titel1.DbType = DbType.String;
+                    command.Parameters.Add(Titel1);
+
+                    DbParameter Nummer1 = sqlFactory.CreateParameter();
+                    Nummer1.ParameterName = "@Nummer";
+                    Nummer1.DbType = DbType.Int32;
+                    command.Parameters.Add(Nummer1);
+
+                    DbParameter Reeks_id1 = sqlFactory.CreateParameter();
+                    Reeks_id1.ParameterName = "@Reeks_id";
+                    Reeks_id1.DbType = DbType.Int32;
+                    command.Parameters.Add(Reeks_id1);
+
+                    DbParameter Uitgeverij_id1 = sqlFactory.CreateParameter();
+                    Uitgeverij_id1.ParameterName = "@Uitgeverij_id";
+                    Uitgeverij_id1.DbType = DbType.Int32;
+                    command.Parameters.Add(Uitgeverij_id1);
+
+
+                    foreach (Strip s in strips)
+                    {
+                        command.Parameters["@Titel"].Value = s.StripTitel;
+                        command.Parameters["@Nummer"].Value = s.StripNr;
+
+                        foreach (KeyValuePair<int, Uitgeverij> dU in dictUitgeverij)
+                        {
+                            if (dU.Value.Naam == s.Uitgeverij.Naam)
+                            {
+                                command.Parameters["@Uitgeverij_id"].Value = dU.Key;
+                            }
+
+                        }
+                        foreach (KeyValuePair<int, Reeks> dR in dictReeks)
+                        {
+                            if (dR.Value.Naam == s.Reeks.Naam)
+                            {
+                                command.Parameters["@Reeks_id"].Value = dR.Key;
+                            }
+                        }
+                        command.ExecuteNonQuery();
+
+
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+            #endregion
+        }
+
+
+        public void AddStrip(Strip strip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Strip> FindAll_ByAuteur(Auteur auteur)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Strip> FindAll_ByReeks(Reeks reeks)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Strip> FindAll_strip()
+        {
+            //    DbConnection connection = getConnection();
+            //    IList<Strip> lg = new List<Strip>();
+            //    string query = "SELECT * FROM dbo.Strip";
+            //    using (DbCommand command = connection.CreateCommand())
+            //    {
+            //        command.CommandText = query;
+            //        connection.Open();
+            //        try
+            //        {
+            //            IDataReader dataReader = command.ExecuteReader();
+            //            while (dataReader.Read())
+            //            {
+            //                int id = (int)dataReader["id"];
+            //                string titel = dataReader.GetString(1); //verschillende methodes om data op te vragen !
+            //                int nr = (int)dataReader["Nummer"];
+            //                int reeks_id = (int)dataReader["Reeks_id"];
+            //                int uitgeverij_id = (int)dataReader["Uitgeverij_id"];
+
+            //            }
+            //            dataReader.Close();
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            Console.WriteLine(ex);
+            //        }
+            //        finally
+            //        {
+            //            connection.Close();
+            //        }
+            //    }
+            //    return lg;
+            throw new NotImplementedException();
+        }
+
+        public Strip FindStripById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveStripById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
