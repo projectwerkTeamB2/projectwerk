b2416bf145095acfac7b01b9f60a12c2847202a9
jeffrey tiberghien <jeffrey.tiberghien@gmail.com>
2020-10-05 16:56:30 +0200

    get connectionstring from config

---
+++ StripcatalogusApp/Datalaag/DbFunctions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Datalaag
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class DbFunctions
+    {
+        static string GetConnectionStringByName(string name)
+        {
+            // Assume failure.
+            string returnValue = null;
+
+            // Look for the name in the connectionStrings section.
+            ConnectionStringSettings settings =
+                ConfigurationManager.ConnectionStrings[name];
+
+            // If found, return the connection string.
+            if (settings != null)
+                returnValue = settings.ConnectionString;
+
+            return returnValue;
+        }
+
+        public static SqlConnection MyConnection()
+        {
+            SqlConnection connection = new SqlConnection(GetConnectionStringByName("projectwerkconnection"));
+            return connection;
+        }
+    }
+
+}
