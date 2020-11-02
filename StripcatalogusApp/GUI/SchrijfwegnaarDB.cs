dd12c10868a40c184205f4d9e412d139fa43cca0
vladik kozynin <liekesliefhebber2000@hotmail.com>
2020-11-02 09:00:36 +0100

    versie 1

---
+++ StripcatalogusApp/GUI/SchrijfwegnaarDB.cs
@@ -0,0 +1,34 @@
+﻿using Businesslaag.Models;
+using Datalaag.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON
+{
+    class SchrijfwegnaarDB
+    {
+        public void allesWegSchijvenNaarDataBank(List<Strip> strips)
+        {
+            StripRepository stripRepo = new StripRepository(System.Configuration.ConfigurationManager.
+    ConnectionStrings["projectwerkconnectionString"].ConnectionString);
+            AuteurRepository auteurRepo = new AuteurRepository(System.Configuration.ConfigurationManager.
+ConnectionStrings["projectwerkconnectionString"].ConnectionString);
+            UitgeverijRepository uitgeverijRepo = new UitgeverijRepository(System.Configuration.ConfigurationManager.
+ConnectionStrings["projectwerkconnectionString"].ConnectionString);
+            ReeksRepository reeksRepo = new ReeksRepository(System.Configuration.ConfigurationManager.
+ConnectionStrings["projectwerkconnectionString"].ConnectionString);
+
+            foreach (Strip str in strips)
+            {
+                foreach(Auteur aut in str.Auteurs)
+                {
+                    auteurRepo.addAuteur(aut);
+                }
+                uitgeverijRepo.addUitgeverij(str.Uitgeverij);
+                reeksRepo.addReeks(str.Reeks);
+                stripRepo.AddStrip(str);
+            }
+        }
+    }
+}
