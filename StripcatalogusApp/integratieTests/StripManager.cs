e4f9cca4da69c8f60c289b2d1ad83fd63d47f8c1
César <cesar.cremery@student.hogent.be>
2020-11-18 14:32:17 +0100

    Unit test

--- StripcatalogusApp/integratieTests/StripManager.cs
+++ StripcatalogusApp/integratieTests/StripManager.cs
@@ -17,17 +17,20 @@ namespace integratieTests
         {
             GeneralManager generalManager = new GeneralManager(new StripRepository(DbFunctions.GetprojectwerkconnectionString()), new AuteurRepository(DbFunctions.GetprojectwerkconnectionString()), new ReeksRepository(DbFunctions.GetprojectwerkconnectionString()), new UitgeverijRepository(DbFunctions.GetprojectwerkconnectionString()));
            int count = generalManager.StripManager.GetAll().Count;
-            Auteur a1 = new Auteur(998, "test");
-            Auteur a2 = new Auteur(999, "test");
+            Auteur a1 = generalManager.AuteurManager.GetById(1);
+            Auteur a2 = generalManager.AuteurManager.GetById(2);
             List<Auteur> auteurs = new List<Auteur>();
             auteurs.Add(a1);
             auteurs.Add(a2);
-            Reeks reeks = new Reeks(999, "testreeks");
-            Uitgeverij Uitgeverij = new Uitgeverij(999, "test");
+            Reeks reeks = generalManager.ReeksManager.GetById(1);
+            Uitgeverij Uitgeverij = generalManager.UitgeverijManager.GetById(1);
             Strip strip = new Strip(9999, "teststrip", 1, auteurs, reeks, Uitgeverij) ;
             generalManager.StripManager.Add(strip);
             int newcount = generalManager.StripManager.GetAll().Count;
             Assert.IsTrue(newcount == count + 1);
+
+            Strip stripFromDb = generalManager.StripManager.GetById(9999);
+            Assert.AreEqual(stripFromDb, strip); 
         }
     }
 }
