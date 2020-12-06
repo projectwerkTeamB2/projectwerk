79618386f10f3f7151fd33bccace77f2e5f23c03
César <cesar.cremery@student.hogent.be>
2020-12-06 13:52:46 +0100

    sqlbuildertest af, error solving

--- StripcatalogusApp/integratieTests/SqlQueryBuilderTest.cs
+++ StripcatalogusApp/integratieTests/SqlQueryBuilderTest.cs
@@ -58,7 +58,55 @@ namespace integratieTests {
 
         [TestMethod]
         public void GetUpdateCommand_ReturnSqlCommand() {
+            List<AuteurDB> auteurDBs = new List<AuteurDB>();
+            auteurDBs.Add(new AuteurDB("test"));
+            StripDB s = new StripDB(1, "test", 1, auteurDBs, new ReeksDB("test"), new UitgeverijDB("test"));
+            var Sql = new SqlQueryBuilder<StripDB>(s);
+            var test = Sql.GetUpdateCommand();
+            Assert.IsInstanceOfType(test, typeof(SqlCommand));
+        }
+        [TestMethod]
+        public void GetUpdateCommand_CheckString() {
+            List<AuteurDB> aut = new List<AuteurDB>();
+            aut.Add(new AuteurDB("test"));
+            aut.Add(new AuteurDB("test2"));
+            StripDB s = new StripDB(1, "Test", 1, aut, new ReeksDB("test"), new UitgeverijDB("test"));
+            var sqlQueryB = new SqlQueryBuilder<StripDB>(s);
+            var test = sqlQueryB.GetUpdateCommand();
+            Assert.IsTrue(test.CommandText.Contains("UPDATE"));
+        }
+        #endregion
 
+        #region Delete
+        [TestMethod]
+        public void GetDeleteCommand_ThrowsException() {
+            StripDB s = new StripDB();
+            var sqlB = new SqlQueryBuilder<StripDB>(s);
+            Assert.ThrowsException<System.Exception>(() => ExecuteCommand(sqlB.GetDeleteCommand()));
+        }
+
+        [TestMethod]
+        public void GetDeleteCommand_CheckIfInstanceOfSqlCommand() {
+            List<AuteurDB> aut = new List<AuteurDB>();
+            aut.Add(new AuteurDB("test"));
+            aut.Add(new AuteurDB("test2"));
+            StripDB s = new StripDB(1, "Test", 1, aut, new ReeksDB("test"), new UitgeverijDB("test"));
+            var sqlQueryB = new SqlQueryBuilder<StripDB>(s);
+            var test = sqlQueryB.GetDeleteCommand();
+            //assert dat Checkt of het een SQlcommand teruggeeft
+            Assert.IsInstanceOfType(test, typeof(SqlCommand));
+        }
+
+        [TestMethod]
+        public void GetDeleteCommand_CheckString() {
+            List<AuteurDB> aut = new List<AuteurDB>();
+            aut.Add(new AuteurDB("test"));
+            aut.Add(new AuteurDB("test2"));
+            StripDB s = new StripDB(1, "Test", 1, aut, new ReeksDB("test"), new UitgeverijDB("test"));
+            var sqlQueryB = new SqlQueryBuilder<StripDB>(s);
+            var test = sqlQueryB.GetInsertCommand();
+            //assert dat Checkt of het een SQlcommand teruggeeft
+            Assert.IsTrue(test.CommandText.Contains("DELETE"));
         }
         #endregion
     }
